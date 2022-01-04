3acf8fdd83acc311baf6fbb99075e4a82f3eb626
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2022-01-04 12:32:34 +0100

    env-dependent frontend base url for login callback

--- src/okta/index.js
+++ src/okta/index.js
@@ -4,7 +4,7 @@ import { OktaAuth } from '@okta/okta-auth-js'
 const oktaSignIn = new OktaSignIn({
   baseUrl: 'https://dev-4978011.okta.com',
   clientId: '0oa3hzj56gAYGbvN25d7',
-  redirectUri: 'http://localhost:3000/login/callback',
+  redirectUri: process.env.VUE_APP_FRONTEND_BASE_URL + '/login/callback',
   authParams: {
     pkce: true,
     issuer: 'https://dev-4978011.okta.com/oauth2/default',
